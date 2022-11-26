3e812d2f50ae033fe69395deb848ff12adc6df10
bigwin22 <gogletaehyung0808@naver.com>
2022-11-26 22:30:22 +0900

    미사일 발사(중복 입력 버그 존재)

--- ConsoleGame/ConsoleGame.c
+++ ConsoleGame/ConsoleGame.c
@@ -19,13 +19,19 @@ int main()
 
     Image images[100] = {
         {"Resource/background/background3.bmp", 0, 0,1.4},
-        {"Resource/Spaceship/Spaceship_fire.bmp",10,1,0.25}
     }; //배열의 첫 원소가 가장 아래 그려진다.
 
     imageLayer.imageCount = 100; //images 배열의 크기보다 작거나 같아야 한다.
     imageLayer.images = images;
 
-	User user = {3, &images[1].x, &images[1].y, 10, 1,1};
+	
+    images[99].fileName = "Resource/Spaceship/Spaceship_fire.bmp";
+	images[99].x = 10;
+	images[99].y = 1;
+	images[99].scale = 0.25;
+
+
+	User user = {3, &images[99].x, &images[99].y, 10, 1,99};
     
 
     while (1)
--- ConsoleGame/Header/Object.h
+++ ConsoleGame/Header/Object.h
@@ -48,14 +48,22 @@ struct Misile_args
 
 void Misile_Func(struct Misile_args Misile_arg)
 {
-	printf("hello");
 	Misile misile = Misile_arg.misile;
 	Image* images = Misile_arg.images;
 
-	images[2].fileName = "Resource/Spaceship/Spaceship_fire.bmp";
+	images[2].fileName = "Resource/Missile/Missile1.bmp";
 	images[2].x = *(misile.x);
 	images[2].y = *(misile.y);
 	images[2].scale = 0.25; 
+	
+	for (int i = 1; Boarder_Check(images[2].x, images[2].y - misile.speed); i++)
+	{
+		images[2].y -= misile.speed;
+		Sleep(10);
+	}
+	images[2].fileName = NULL, images[2].x = 0, images[2].y = 0, images[2].scale = 0;
+	
+	return;
 	
 }
 
